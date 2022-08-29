e413e79f3a43256dd946ca9f1802b7f840ee1af0
Octavio Peralta <octi.pro20@gmail.com>
2022-08-29 00:37:23 -0300

    defincicion modelo Post y creacion index.html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
+from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    POST_CATEGORIES = [
+        ('TECH', 'Technology'),
+        ('POLITICS', 'Politics'),
+        ('TRAVEL', 'Travel'),
+        ('SPORT', 'Sports')
+    ]
+
+    title = models.CharField(max_length=250)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(
+        max_length=20, choices=POST_CATEGORIES, default='TECH')
+
+    def __str__(self):
+        return self.title
